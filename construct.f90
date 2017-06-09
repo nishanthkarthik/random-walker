92c131c6443fafe296f9516d50a7095161d7f6a4
Karthik Nishanth <nishanthkarthik@live.com>
2017-06-09 15:32:58 +0530

    bugfix - use displacement offsets as int8, not default int4

--- construct.f90
+++ construct.f90
@@ -7,29 +7,40 @@ module construct
 
     integer :: MPI_WALKER_TYPE    
     type :: walkunit
-        integer(8) :: index
-        integer(8) :: position
-        integer(4) :: maxstep
+        integer :: index
+        integer :: position
+        integer :: maxstep
     end type walkunit
 
 contains
-    
+
     subroutine create_walker_type()
         implicit none
-        include 'mpif.h'
         integer :: lblock(3) = (/ 1, 1, 1 /)
-        integer :: types(3) = (/ MPI_INTEGER8, MPI_INTEGER8, MPI_INTEGER4 /)
-        integer :: offsets(3), int8offset, lowerbound
+        integer :: types(3) = (/ MPI_INTEGER, MPI_INTEGER, MPI_INTEGER /)
+        integer(kind=MPI_ADDRESS_KIND) :: offsets(3), intoffset, lowerbound
         integer :: ierr
         
-        call MPI_Type_Get_Extent(MPI_INTEGER8, lowerbound, int8offset)
+        call MPI_Type_Get_Extent(MPI_INTEGER, lowerbound, intoffset)
         offsets(1) = 0
-        offsets(2) = int8offset
-        offsets(3) = 2 * int8offset
+        offsets(2) = intoffset
+        offsets(3) = 2 * intoffset
+        ! offsets(1) = 0
+        ! offsets(2) = sizeof(ierr)
+        ! offsets(3) = sizeof(ierr) * 2
 
         call MPI_Type_Create_Struct(3, lblock, offsets, types, MPI_WALKER_TYPE, ierr)
+        call throw_mpi(ierr)
         call MPI_Type_Commit(MPI_WALKER_TYPE, ierr)
-        print *, MPI_WALKER_TYPE
+        call throw_mpi(ierr)
     end subroutine create_walker_type
+
+    subroutine throw_mpi(ierr)
+        implicit none
+        integer :: ierr
+        if (ierr /= 0) then
+            call MPI_Abort(MPI_COMM_WORLD, ierr, ierr)
+        endif
+    end subroutine throw_mpi
 
 end module construct
